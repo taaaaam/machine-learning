1bbab7901a325d6132d089d2f2e63d111194fd3b
tamvu2104 <v.tamvm3@vinbigdata.com>
2022-07-29 15:33:50 +0700

    Initial commit

---
+++ Liver_Segmentation_UNet/train.py
@@ -0,0 +1,134 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import UNET
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_prediction_images,
+)
+
+# Hyperparameters etc.
+LEARNING_RATE = 1e-3
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 30
+NUM_EPOCHS = 10
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 256 #160  
+IMAGE_WIDTH = 256 #512 originally
+PIN_MEMORY = True
+
+LOAD_MODEL = False
+
+TRAIN_IMG_DIR = "./data/train_images_total/" #more --> total
+TRAIN_MASK_DIR = "./data/train_masks_total/"
+VAL_IMG_DIR = "./data/val_images_total/"
+VAL_MASK_DIR = "./data/val_masks_total/"
+
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device=DEVICE)
+        targets = targets.float().unsqueeze(1).to(device=DEVICE)
+
+        # forward
+        with torch.cuda.amp.autocast(): #autocast allows regions of script to run in mixed precision    
+            predictions = model(data)   #allows for improved performance while maintaining accuracy
+            loss = loss_fn(predictions, targets)
+
+        # backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        # update tqdm loop
+        loop.set_postfix(loss=loss.item())
+
+
+def main():
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            #A.Rotate(limit=35, p=1.0),
+            #A.HorizontalFlip(p=0.5),
+            #A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            #A.RandomRotate90(p=0.2), #new
+            ToTensorV2(),
+        ],
+    )
+
+    val_transforms = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+        val_transforms,
+        NUM_WORKERS,
+        PIN_MEMORY,
+    )
+
+    
+
+
+    #train_loader data.size = [30, 3, 256, 256]
+    #train_laoder targets.size = [30, 256, 256]
+
+
+    # if LOAD_MODEL:
+    #     load_checkpoint(torch.load("my_checkpoint.pth.tar"), model)
+
+
+    check_accuracy(val_loader, model, device=DEVICE)
+    scaler = torch.cuda.amp.GradScaler()
+
+    for epoch in range(NUM_EPOCHS):
+        train_fn(train_loader, model, optimizer, loss_fn, scaler)
+
+        # save model
+        checkpoint = {
+            "state_dict": model.state_dict(),
+            "optimizer":optimizer.state_dict(),
+        }
+        save_checkpoint(checkpoint)
+
+        # check accuracy
+        check_accuracy(val_loader, model, device=DEVICE)
+        
+        # print some examples to a folder
+        save_prediction_images(
+            val_loader, model, folder="./saved_images/", device=DEVICE
+        )
+
+
+if __name__ == "__main__":
+    main()
